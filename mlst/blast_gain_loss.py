f27693906a7fd3626f60cc98e35d2a6b615ce677
Rinoahu <xiaohu@iastate.edu>
2020-04-03 17:46:36 -0600

    try to add remove function

---
+++ mlst/blast_gain_loss.py
@@ -0,0 +1,67 @@
+#!usr/bin/env python
+from Bio import SeqIO
+import os
+import sys
+
+try:
+    qry, dir=sys.argv[1: 3]
+except:
+    print('python this.py qry dir')
+    raise SystemExit()
+
+
+def overlap(s1, e1, s2, e2):
+    over_lap = min(e1, e2) - max(s1, s2) + 1
+    return over_lap
+
+
+f = open(qry, 'r')
+seqs_dict = SeqIO.to_dict(SeqIO.parse(f, 'fasta'))
+f.close()
+
+
+output = []
+for ref in [dir + '/' + elem for elem in os.listdir(dir) if elem.endswith('.fasta')]:
+    #os.system('legacy_blast.pl formatdb -i %s -p F | tee log.txt'%ref)
+    os.system('legacy_blast.pl blastall -m 8 -e 1e-10 -a 4 -p blastn -i %s -d %s -o %s.blast8 | tee log.txt'%(qry, ref, ref))
+
+    blast_flt = {}
+    f = open(ref + '.blast8', 'r')
+    for i in f:
+        j = i[:-1].split('\t')
+        qid, sid, idy, aln, mis, gop, qst, qed, sst, sed, evl, sco = j
+        qst, qed, sst, sed = map(int, [qst, qed, sst, sed])
+        qln = len(seqs_dict[qid])
+        cov = abs(qed-qst) * 1./ qln
+        idy = float(idy)
+        if idy >= 70 and cov >= .5:
+            tpd = qid.split('__')[1]
+            try:
+                blast_flt[sid].append([tpd, sid, idy, qst, qed, sst, sed])
+            except:
+                blast_flt[sid] = [[tpd, sid, idy, qst, qed, sst, sed]]
+
+    genes = []
+    for values in blast_flt.values():
+        values.sort(key = lambda x: x[5])
+        gene = []
+        for i in values:
+            if len(gene) == 0:
+                gene.append(i)
+            else:
+                s1, e1 = gene[-1][-2:]
+                s2, e2 = i[-2:]
+                if overlap(s1, e1, s2, e2) > 0 and gene[-1][2] < i[2]:
+                    gene[-1] = i
+                else:
+                    gene.append(i)
+
+        genes.extend(gene)
+
+    print('break 60', len(set([elem[0] for elem in genes])))
+
+    output.append([ref, [elem[0] for elem in genes]])
+
+f.close()
+
+print('break 67', output)
--- mlst/run_all.sh
+++ mlst/run_all.sh
@@ -46,35 +46,20 @@ fi
 # download vf
 mkdir -p vf_db
 srst2_cls=$PWD/tools/srst2/database_clustering
-taxon=Haemophilus
-cd vf_db
+#taxon=Haemophilus
+#cd vf_db
 #wget -c http://www.mgc.ac.cn/VFs/Down/VFDB_setB_nt.fas.gz
 #gunzip VFDB_setB_nt.fas.gz
-python $srst2_cls/VFDBgenus.py --infile VFDB_setB_nt.fas --genus $taxon
+#python $srst2_cls/VFDBgenus.py --infile VFDB_setB_nt.fas --genus $taxon
 #cd-hit -i $taxon\.fsa -o $taxon\_cdhit90 -c 0.9 > $taxon\_cdhit90.stdout
 #python $srst2_cls/VFDB_cdhit_to_csv.py --cluster_file $taxon\_cdhit90.clstr \
 # --infile $taxon\.fsa --outfile $taxon\_cdhit90.csv
 #python $srst2_cls/csv_to_gene_db.py -t $taxon\_cdhit90.csv \
 # -o $taxon\_VF_clustered.fasta -s 5
-cd ..
-
-#exit 0;
+#cd ..
 
 # install ssuis st
 #git clone https://github.com/streplab/SsuisSerotyping_pipeline
-
-# test ssuis st
-rm -rf recN/ Serotyping/ MLST/ Virulence/
-F_pe=_R1
-R_pe=_R2
-#perl ./SsuisSerotyping_pipeline/Ssuis_serotypingPipeline.pl --fastq_directory /home/xiaohu/research/mlst_test/data/gp_b4_raw_reads/M-B4-161_S24_L001_output --scoreName Score_output_type1 --forward $F_pe --reverse $R_pe &> log.txt
-
-
-F_pe=_S3_L001_R1_001
-R_pe=_S3_L001_R2_001
-#perl ./SsuisSerotyping_pipeline/Ssuis_serotypingPipeline.pl --fastq_directory $PWD/serotyping_testing/type1over2 --scoreName Score_output_type1over2 --forward $F_pe --reverse $R_pe &> log.txt
-
-#exit 0;
 
 # test srst2
 #srst2 --output test --input_pe $F_pe $R_pe --mlst_db ./mlst_db/Streptococcus_suis.fasta --mlst_definitions ./mlst_db/ssuis.txt --mlst_delimiter '_'
@@ -83,11 +68,11 @@ R_pe=_S3_L001_R2_001
 # get mlst database
 mkdir -p mlst_db
 cd mlst_db
-#getmlst.py --species "Streptococcus suis"
+getmlst.py --species "Streptococcus suis"
 cd ..
 
 # adapter
-ad_seq=/home/xiaohu/research/mlst_test/data/supporting_DB/adapters/NexteraPE-PE.fa
+ad_seq=$PWD/data/supporting_DB/adapters/NexteraPE-PE.fa
 
 cdir=$PWD
 # trim the reads
@@ -112,7 +97,7 @@ do
     # trim the data
     phred=`zcat $is | head -n 40 | awk '{if(NR%4==0) printf("%s",$0);}' |  od -A n -t u1 | awk 'BEGIN{min=100;max=0;}{for(i=1;i<=NF;i++) {if($i>max) max=$i; if($i<min) min=$i;}}END{if(max<=74 && min<59) print "phred33"; else if(max>73 && min>=64) print "phred64"; else if(min>=59 && min<64 && max>73) print "Solexa+64"; else print "Unknown score encoding";}'`
     #echo $phred
-    #trimmomatic PE -threads 16 -$phred $F_pe $R_pe $F_pe\_clean.fq.gz $F_pe\_unpaired.fq.gz $R_pe\_clean.fq.gz $R_pe\_unpaired.fq.gz ILLUMINACLIP:$ad_seq:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 HEADCROP:15
+    trimmomatic PE -threads 16 -$phred $F_pe $R_pe $F_pe\_clean.fq.gz $F_pe\_unpaired.fq.gz $R_pe\_clean.fq.gz $R_pe\_unpaired.fq.gz ILLUMINACLIP:$ad_seq:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36 HEADCROP:15
 
     # prepare reads
     #echo $PWD
