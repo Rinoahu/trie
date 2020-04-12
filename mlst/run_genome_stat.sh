0e97ea8af0fdcae74dcf9aed0722db120c5cfef7
Rinoahu <xiaohu@iastate.edu>
2020-04-11 20:39:49 -0600

    try to add remove function

--- mlst/run_genome_stat.sh
+++ mlst/run_genome_stat.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-for i in `find ../../../data/ss_b*_raw_reads/*_output -name *_scaffolds.fsa`
+genome=$1
+reads=$2
+
+#for i in `find ../../../data/ss_b*_raw_reads/*_output -name *_scaffolds.fsa`
+for i in `find $genome -name *_scf.fsa`
 do
     x=`getN50 $i | awk '{printf "%s" (NR%5==0?RS:FS),$1" "$2}'`
-    j=`echo $i | awk -F"_scaffolds.fsa" '{print $1}' | awk -F"/" '{print $NF}'`
+    j=`echo $i | awk -F"_scf.fsa" '{print $1}' | awk -F"/" '{print $NF}'`
     hd=`echo $x | cut -f1,3,5,7,9,11 -d\ `
     value=`echo $x | cut -f2,4,6,8,10,12 -d\ `
     echo "Plate_num Raw_Seq_Name Read_Pair_Counts Coverage Assembly_file" $hd
     break
 done
 
-for i in `find ../../../data/ss_b*_raw_reads/*_output -name *_scaffolds.fsa`
+
+#for i in `find ../../../data/ss_b*_raw_reads/*_output -name *_scaffolds.fsa`
+for i in `find $genome -name *_scf.fsa`
 do
     fn=`echo $i | awk -F"/" '{print $NF}'`
     x=`getN50 $i | awk '{printf "%s" (NR%5==0?RS:FS),$1" "$2}'`
-    j=`echo $i | awk -F"_scaffolds.fsa" '{print $1}'`
+    j=`echo $i | awk -F"_scf.fsa" '{print $1}' | awk -F"/" '{print $NF}'`
     raw_name=`echo $j | awk -F"/" '{print $NF}'`
     value=`echo $x | cut -f2,4,6,8,10,12 -d\ `
     size=`echo $x | cut -f4 -d\ `
-    M=`zcat $j\_*clean* | awk 'NR%4==2' | wc -c -l`
+
+    M=`zcat $reads/$j\_*fastq.gz | awk 'NR%4==2' | wc -c -l`
     rows=`echo $M | cut -f1 -d\ `
     N=`echo $M | cut -f2 -d\ `
     #echo $rows $N
